7f119953b3a4cb13d109d512122ce39686024449
Mete <126513337+mmetem55@users.noreply.github.com>
2026-09-13 19:35:11 +0300

    Update make.sh

--- make.sh
+++ make.sh
@@ -22,7 +22,7 @@ if [ "$1" = "arm" ];then
 	make ARCH=arm DEVICE_TREE="$3" -j4
 
 elif [ "$1" = "arm64" ]; then
-	export PATH=$PATH:$(pwd)/../prebuilts/gcc/linaro-x86/aarch64/gcc-linaro-4.8/gcc-linaro-4.8-2015.06-x86_64_aarch64-linux-gnu/bin
+	export PATH=$PATH:$(pwd)/../prebuilts/gcc/linaro-x86/aarch64/gcc-linaro-4.9.4-2017.01-x86_64_aarch64-linux-gnu/bin
 	export CROSS_COMPILE=aarch64-linux-gnu-
 	make distclean
 	make ARCH=arm "$2"_defconfig
