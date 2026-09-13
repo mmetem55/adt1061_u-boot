4f72bd61576d05f6fefdaa31409d3e502df04366
Mete <126513337+mmetem55@users.noreply.github.com>
2026-09-13 19:21:00 +0300

    Update make-debug.sh

--- make-debug.sh
+++ make-debug.sh
@@ -41,3 +41,11 @@ elif [ "$1" = "x86" ]; then
 else
     echo "not suppported arch"
 fi
+
+if [ $? -eq 0 ]; then
+	echo "Signing U-Boot binary..."
+	python3 scripts/signer/v2/sign_uboot.py out/u-boot-dtb.bin out/u-boot-sign.bin
+else
+	echo "Build failed. Skipping signing."
+	exit 1
+fi
